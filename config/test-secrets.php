a8dbd23eee6fbb0f5eb35ebf55c04c4de35ab7bc
inv3ntor01 <inv3ntor01@users.noreply.github.com>
2026-06-13 01:43:30 +0800

    test: add config file with secrets (should be caught by gitleaks)

---
+++ config/test-secrets.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'aws_access_key' => 'AKIAIOSFODNN7EXAMPLE',
+    'aws_secret_key' => 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
+    'database_url' => 'postgres://admin:supersecretpassword123@db.example.com:5432/larakube',
+];
